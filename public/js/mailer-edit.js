012dcc8d0f3753b657d88a46b6430a649f1daaba
Jonathan Buma <johnny.buma@gmail.com>
2015-06-09 20:21:11 -0700

    Added basic contact form validation

---
+++ public/js/mailer-edit.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    var from,to,subject,text;
+    var to = "johnny.buma@gmail.com";
+    
+    $("#send_email").click(function(){
+            if ("#from" != " ") {from=$("#from").val(); }
+            to= to;
+            subject=$("#subject").val();
+            text= "This message was sent from" + " " + $("#from").val() + " " + $("#content").val();
+            $("#message").text("Sending E-mail...Please wait");
+            $.get("/send",{from:from,to:to,subject:subject,text:text},function(data){
+            if(data=="sent")
+            {
+                $("#message").empty().html
+
+            (" Your message has been sent, thank you for your interest !");
+            }
+    
+        });
+        });
+});
